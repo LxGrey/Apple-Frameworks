8837043002f62c8d41a0ad2d303f70f24ca3484d
LxGrey <lx_grey@icloud.com>
2021-07-14 12:52:53 -0500

    Initial setup, Framework struct, Mockdata file

---
+++ Apple-Frameworks/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by Shelton Scharhag on 7/14/21.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
